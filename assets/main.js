3bb9d2c2d8ffc820b45618e6c17a86dd86d14cb7
Wachira Nduati <wachiranduati@gmail.com>
2020-08-31 20:55:34 +0300

    create signup screen

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,15 @@
 $('.startedscreen').hide()
+$('.accountscreen').hide()
 $('#gettingStratedBtn').click(function(){
     $('.splashscreen').hide()
     $('.startedscreen').show()
+})
+
+$(".signupBtn").click(function(){
+    $('.startedscreen').hide()
+    $('.accountscreen').show()
+})
+
+$('.signInBtn').click(function(){
+    alert('sign in button')
 })
